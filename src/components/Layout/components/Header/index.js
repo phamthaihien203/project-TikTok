62284ac6e563830bbdb4b1715efc8c5a8c517fb3
phamthaihien203 <phamthaihien.203@gmail.com>
2024-08-13 16:19:11 +0700

    update 13/8

--- src/components/Layout/components/Header/index.js
+++ src/components/Layout/components/Header/index.js
@@ -1,6 +1,16 @@
-import styles from './Header.module.scss
+import classNames from 'classnames/bind';
+import styles from './Header.module.scss';
+
+const cx = classNames.bind(styles);
 function Header() {
-    return <h2>Header</h2>;
+    return (
+        <header className={cx('wrapper')}>
+            <div className={cx('inner')}>
+                {/* Logo */}
+                {/* Search */}
+            </div>
+        </header>
+    );
 }
 
 export default Header;
